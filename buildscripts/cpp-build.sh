9fbbc978ad1291b8eed6ec9ca54e092ed8591cba
Michał Rybiński <54797573+michal-at-travisci@users.noreply.github.com>
2020-03-19 12:52:10 +0100

    fixing  bash if-then-else syntax

--- buildscripts/cpp-build.sh
+++ buildscripts/cpp-build.sh
@@ -3,10 +3,11 @@
 os=$(uname -a | cut -c1-7 | tr '[:upper:]' '[:lower:]' )
 echo $os
 # build app using Makefile and make binary executable
-if [ "$os" == "freebsd" ] {
+if [ "$os" == "freebsd" ] 
+then
   gmake demo && chmod +x ../build/demo 
-} else {
+else 
   make demo && chmod +x ../build/demo 
-}
+fi
 # run application we've just built
 ../build/demo
