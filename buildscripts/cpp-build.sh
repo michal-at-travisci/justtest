28c898ea2c57a87d8f85d0129eec2224a6fcbb87
Michał Rybiński <54797573+michal-at-travisci@users.noreply.github.com>
2020-03-19 12:45:07 +0100

    Update cpp-build.sh

--- buildscripts/cpp-build.sh
+++ buildscripts/cpp-build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+os = $(uname -a | cut -c1-7 | tr '[:upper:]' '[:lower:]' )
+echo "$os"
 # build app using Makefile and make binary executable
-make demo && chmod +x ../build/demo 
-
+if "$os" == "freebsd" {
+  gmake demo && chmod +x ../build/demo 
+} else {
+  make demo && chmod +x ../build/demo 
+}
 # run application we've just built
 ../build/demo
